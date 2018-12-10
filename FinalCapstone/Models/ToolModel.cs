08574fbfad195e07fb2defdeda07aa325060b2db
Nicholas Hawk <night.hawk.20@hotmail.com>
2018-12-10 13:09:57 -0500

    Created Tool and User Models

---
+++ FinalCapstone/Models/ToolModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalCapstone.Models
+{
+    public class Tool
+    {
+            public string Brand { get; set; }
+            public string ToolName { get; set; }
+            public string Description { get; set; }
+            public bool CheckedOut { get; set; }
+            public string ImageName
+            {
+                get => ImageName = ImageName;
+                set
+                {
+                    ImageName = $"{Brand} {ToolName}";
+                }
+            }
+    }
+}
