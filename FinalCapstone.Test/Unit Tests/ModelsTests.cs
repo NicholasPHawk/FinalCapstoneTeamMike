1c41ba9c5681f4763226b8e91fcbf2a872585ab3
Nicholas Hawk <night.hawk.20@hotmail.com>
2018-12-12 11:31:41 -0500

    Added unit test to test the creation of the image names, adjusted the set field of the ImageName property to accurately reflect the image filenames

---
+++ FinalCapstone.Test/Unit Tests/ModelsTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.SqlClient;
+using System.Linq;
+using FinalCapstone.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace FinalCapstone.Test.Unit_Tests
+{
+    [TestClass]
+    public class ModelsTests
+    {
+        [TestMethod]
+        public void ToolImageNameTest()
+        {
+            Tool tool = new Tool()
+            {
+                Brand = "DewaltDisney",
+                ToolName = "Table Saw"
+            };
+
+            Assert.AreEqual("DewaltDisneyTable Saw", tool.ImageName);
+        }
+    }
+}
